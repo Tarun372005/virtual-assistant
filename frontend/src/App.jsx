5a4246397a8362048faec75283b4ad1a84001fa2
Tarun372005 <teotiat00@gmail.com>
2025-09-05 12:15:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Route , Routes, Navigate} from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
-import { userDataContext } from './context/userContext'
+import { userDataContext } from './context/UserContext'
 import Home from './pages/Home'
 import Customize2 from './pages/Customize2'
 
